2e4cca33300fa2023e865016ebc899ba32181f9f
aomkar2421 <omkarjagtap1651@gmail.com>
2024-03-17 23:28:25 +0530

    17 March

---
+++ JokesExtension/index.js
@@ -0,0 +1,28 @@
+fetch("https://v2.jokeapi.dev/joke/Programming")
+  .then((data) => data.json())
+  .then((jokesData) => {
+    const setup = jokesData.setup;
+    const delivery = jokesData.delivery;
+    const joke = jokesData.joke;
+    const p = document.getElementById("a");
+
+    if(setup == undefined || delivery == undefined){
+        p.innerHTML = joke;
+    }else{
+        p.innerHTML = setup + " " + delivery;
+    }
+});
+
+
+// fetch("https://api.api-ninjas.com/v1/jokes?limit=1")
+//   .then((data) => data.json())
+//   .then((jokesData) => {
+//     const joke = jokesData.joke;
+//     const p = document.getElementById("a");
+//     a.innerHTML = joke;
+//     console.log(joke)
+// });
+
+
+
+
